68178a49c3cc9ecdd4b4194e7ca5803855948e56
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-06-03 10:07:19 +0800

    初始化

---
+++ module1/src/main/java/com/future/demo/multiple/module1/Module1Util.java
@@ -0,0 +1,7 @@
+package com.future.demo.multiple.module1;
+
+public class Module1Util {
+    public static String getString() {
+        return "这是模块1 getString函数";
+    }
+}
---
+++ module2/src/main/java/com/future/demo/multiple/module2/Module2Util.java
@@ -0,0 +1,7 @@
+package com.future.demo.multiple.module2;
+
+public class Module2Util {
+    public static String getString() {
+        return "这是模块2 getString函数";
+    }
+}
